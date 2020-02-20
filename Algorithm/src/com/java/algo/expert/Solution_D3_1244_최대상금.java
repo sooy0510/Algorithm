73c95437fbe3a148c3751a7827e1fafcbc7f04ff
qkdqnwpwp <qkdqnwpwp@naver.com>
2020-02-20 14:14:04 +0900

    200220 | Expert_D3_1244_최대상금

--- Algorithm/src/com/java/algo/expert/Solution_D3_1244_최대상금.java
+++ Algorithm/src/com/java/algo/expert/Solution_D3_1244_최대상금.java
@@ -3,6 +3,7 @@ package com.java.algo.expert;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution_D3_1244_최대상금 {
@@ -12,12 +13,15 @@ public class Solution_D3_1244_최대상금 {
 	static int S;	//교환횟수
 	static int len;
 	static int MAX = 0;
+	static HashSet<String> s = new HashSet<String>();;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();;
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 
+		
+		
 		for(int test=1; test<=T; test++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
@@ -35,7 +39,7 @@ public class Solution_D3_1244_최대상금 {
 
 			//System.out.println(Arrays.toString(numbers));
 			
-			permutation(0, 0);	//처리할 인덱스, 교환횟수
+			permutation(S);	//잔여 교환횟수
 			sb.append("#"+test+" "+MAX+"\n");
 		}
 		System.out.println(sb);
@@ -43,71 +47,32 @@ public class Solution_D3_1244_최대상금 {
 		
 	}
 
-	private static void permutation(int index, int cnt) {
+	private static void permutation(int S) {
 		
-//		if(index == len) {
-//			//System.out.println(Arrays.toString(numbers));
-//			if(S - cnt > 0) {
-//				//순열이 완성되었는데 교환횟수가 남았다면
-//			}else {
-//				int sum = 0;
-//				int mul = 1;
-//				for(int i=len-1; i>=0; i--) {
-//					sum += numbers[i] * mul;
-//					mul *= 10;
-//				}
-//				MAX = Math.max(sum, MAX);
-//				return;
-//			}
-			
-			
-		if(index == len) {
-			//System.out.println(Arrays.toString(numbers));
-			//System.out.println(Arrays.toString(numbers)+" / "+cnt);
-//			if(S-cnt>0) {
-//				len = S-cnt;
-//				permutation(0, 0);
-//			}else {
-//				check(cnt);
-//				return;
-//			}
-			
-			
-			check(cnt);
+		int val = 0;
+		for(int i=0; i<len; i++) {
+			val = val*10 + numbers[i];
+		}
+		
+		if(s.contains(""+val+S)) {
 			return;
 		}
-			
 		
-		
-		//자기포함 뒤에있는것들만 교환대상임
-		for(int i=index; i<len; i++) {
-			//index 위치와 i를 swap
-			swap(index, i);
-			if(cnt == S) {
-				check(cnt);
-				continue;
-			}
-			permutation(index+1, cnt+1);
-			//System.out.println(Arrays.toString(numbers)+" / "+cnt);
-//			if(cnt == S) {
-//				check(cnt);
-//				continue;
-//			}
-			swap(index, i);	//원상태
+		s.add(""+val+S);
+		if (S == 0) {
+			if (MAX < val) MAX = val;
+			return;
 		}
-	}
-
-	private static void check(int count) {
-		//if(count == -S) {
-			System.out.println(Arrays.toString(numbers)+" / "+count);
-			int sum = 0;
-			int mul = 1;
-			for(int i=len-1; i>=0; i--) {
-				sum += numbers[i] * mul;
-				mul *= 10;
+		
+		
+		//모든 경우의 수를 다 따져봐야함
+		for(int i=0; i<len-1; i++) {
+			for(int j=i+1; j<len; j++) {
+				swap(i,j);
+				permutation(S-1);
+				swap(i, j);
 			}
-			MAX = Math.max(sum, MAX);
-		//}
+		}
 	}
 
 	private static void swap(int a, int b) {
