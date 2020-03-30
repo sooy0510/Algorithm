1d01ca63340159bed5b00102ec9df22c355b8b30
이수연 <qkdqnwpwp@naver.com>
2020-03-30 23:23:32 +0900

    200330 | 백준_G5_1600_말이되고픈원숭이

--- Algorithm/src/com/java/algo/baekjoon/Main_B_G5_1600_말이되고픈원숭이.java
+++ Algorithm/src/com/java/algo/baekjoon/Main_B_G5_1600_말이되고픈원숭이.java
@@ -8,130 +8,73 @@ import java.util.StringTokenizer;
 
 public class Main_B_G5_1600_말이되고픈원숭이 {
 
-	private static int K;
-	private static int N,M;
-	private static int[][] map;
-	private static int MIN;
-	private static boolean[][] visited;
-
+	
+	static int map[][];
+	static int row,col,endX,endY,count;
+	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		K = Integer.parseInt(br.readLine()); 
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		count = Integer.parseInt(in.readLine());
+		StringTokenizer st= new StringTokenizer(in.readLine(), " ");
+		col = Integer.parseInt(st.nextToken());
+		row = Integer.parseInt(st.nextToken());
+		endX = row-1;
+		endY = col-1;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+		map = new int[row][col];
 		
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		MIN = Integer.MAX_VALUE;
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			for (int j = 0; j < M; j++) {
-				map[i][j] = st.nextToken().charAt(0)-'0';
+		for (int i = 0; i < row; i++) {
+			st = new StringTokenizer(in.readLine()," ");
+			for (int j = 0; j < col; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		if(N==1 && M==1) {
-			System.out.println(0);
-			return;
-		}
+		System.out.println(move());
 		
-		bfs(0,0);
-		System.out.println(MIN == Integer.MAX_VALUE? -1 : MIN);
 	}
 
-	//우하좌상
-	static int[] di = {0,1,0,-1};
-	static int[] dj = {1,0,-1,0};
-	//horse
-	static int[] hi = {1,2,2,1,-1,-2,-2,-1};
-	static int[] hj = {2,1,-1,-2,-2,-1,1,2};
-	
-	private static void bfs(int si, int sj) {
-
-		Queue<Point> queue = new LinkedList<Point>();
+	static int[][][] direction = {
+			{ {-1,0},{1,0},{0,-1},{0,1} }, // 원숭이의 움직임 (상하좌우)
+			{ {-1,-2},{1,-2},{-2,-1},{2,-1},{-2,1},{2,1},{-1,2},{1,2}}, // 말의 움직임(8방)
+	};
+	private static int move() {
+		int moveCnt=0, temp[]=null, x=0,y=0,nx=0,ny=0,cnt=0;
+		boolean[][][] visited = new boolean[count+1][row][col]; // 31*200*200 : 1,200,000
+		Queue<int[]> queue = new LinkedList<int[]>();
+		visited[count][0][0] = true;
+		queue.offer(new int[] {count,0,0,0}) ; //남은말의움직임횟수,x,y,이동동작횟수 
+		while (!queue.isEmpty()) {
 			
-		visited[si][sj] = true;
-		queue.add(new Point(si,sj,K));
-		
-		int result=-1;
-		while(!queue.isEmpty()) {
-			int size = queue.size();
-			result++;
-			while(size --> 0) {
-				Point now = queue.poll();
+			temp  = queue.poll();
+			cnt = temp[0];
+			x = temp[1];
+			y = temp[2];
+			moveCnt = temp[3];
+			
+			if(x==endX && y==endY) return moveCnt;
+			
+			for (int h = 0; h < 2; h++) { // h:0 ==>  원숭이 ,  1: 말 
 				
-				if(now.i == N-1 && now.j== M-1) {
-					MIN = Math.min(result, MIN);
-					return;
+				if(h==1) { // 말 
+					if(cnt==0) break; //  말의 움직임을 다한 경우 
+					else cnt--; // 말의 움직임이 남은 경우 횟수 차감 
 				}
 				
-				for(int x=0; x<2; x++) {
-					switch (x) {
-					case 0:	//인접
-						for(int d=0; d<4; d++) {
-							int next_i = now.i + di[d];
-							int next_j = now.j + dj[d];
-
-//							if(next_i == N-1 && next_j== M-1) {
-//								MIN = Math.min(result, MIN);
-//								return;
-//							}
-							
-							if(next_i < 0 || next_i >= N || next_j < 0 || next_j >= M)
-								continue;
-							
-							if(map[next_i][next_j] == 1 || visited[next_i][next_j]) 
-								continue;
-							
-							//System.out.println("인접 // next.i : "+next_i+ " / next.j : "+next_j + "/ result : "+result);
-
-							visited[next_i][next_j] = true;
-							queue.add(new Point(next_i, next_j, now.k));
-							
-						}
-						break;
-
-					case 1:	//말
-						if(now.k <= 0) break;
-						for(int d=0; d<8; d++) {
-							int next_i = now.i + hi[d];
-							int next_j = now.j + hj[d];
-							
-
-							if(next_i < 0 || next_i >= N || next_j < 0 || next_j >= M)
-								continue;
-							
-							if(map[next_i][next_j] == 1 || visited[next_i][next_j])
-								continue;
-							
-//							if(next_i == N-1 && next_j== M-1) {
-//								MIN = Math.min(result, MIN);
-//								return;
-//							}
-							
-							//System.out.println("말 // next.i : "+next_i+ " / next.j : "+next_j + "/ result : "+result);
-							visited[next_i][next_j] = true;
-							queue.add(new Point(next_i, next_j, now.k-1));
-						}
-						
-						break;
+				for (int d = 0; d < direction[h].length; d++) {
+					
+					nx = x + direction[h][d][0];
+					ny = y + direction[h][d][1];
+					
+					if(nx>=0 && nx<row && ny>=0 && ny<col && map[nx][ny] ==0 && !visited[cnt][nx][ny]) {
+						visited[cnt][nx][ny] = true;
+						queue.offer(new int[] {cnt,nx,ny,moveCnt+1});
 					}
+					
 				}
 			}
 		}
-		
-	}
-	
-	static class Point{
-		int i,j,k;
-		Point(int i, int j, int k){
-			this.i = i;
-			this.j = j;
-			this.k = k;
-		}
+		return -1;
 	}
 
 }
