2d4afc301faea465990faf4766e1b01900a16286
이수연 <qkdqnwpwp@naver.com>
2020-04-30 23:54:41 +0900

    200430 | 백준_G5_16234_인구이동

--- Algorithm/src/com/java/algo/baekjoon/Main_B_G5_16234_인구이동.java
+++ Algorithm/src/com/java/algo/baekjoon/Main_B_G5_16234_인구이동.java
@@ -11,10 +11,14 @@ public class Main_B_G5_16234_인구이동 {
 
 	private static int N,L,R;
 	private static int[][] map;
-	private static int[][] visited;
-	private static int ans;
-	private static int idx;
+	private static boolean[][] visited;
+	private static boolean[][] newVisited;
 
+	//상하좌우
+	static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+	private static int count;	//이동한 횟수
+	private static int sum, result, change;
+	
 	private static class Dot{
 		int i,j;
 		Dot(int i, int j){
@@ -22,6 +26,7 @@ public class Main_B_G5_16234_인구이동 {
 			this.j = j;
 		}
 	}
+	
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,132 +36,58 @@ public class Main_B_G5_16234_인구이동 {
 		R = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][N];
-		visited = new int[N][N];
-		ans = 0;
-		
+		visited = new boolean[N][N];
+		newVisited = new boolean[N][N];
+		count = 0;
+		sum = 0;
+		result = 0;
+		change = 0;
+	
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		while(!start()) {
-			System.out.println("hi");
-			//ans++;
-		}
-		System.out.println(ans);
-	}
-
-	//상하좌우
-	static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-	private static boolean start() {
-		int ni, nj;
-		int diff;
-		boolean isStop = true;
-		
-		//visited 초기화
-			for(int v=0; v<N; v++) {
-				//Arrays.fill(visited[v], false);
-				Arrays.fill(visited[v], 0);
-			}
-		
-		idx = -1;
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				for(int d=0; d<4; d++) {
-					ni = i + dir[d][0];
-					nj = j + dir[d][1];
+		//dfs
+		while(true) {
+			int temp = change;
+			visited = new boolean[N][N];
+			newVisited = new boolean[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(newVisited[i][j] == true) {
+						continue;
+					}
 					
-					if(ni >= 0 && nj >= 0 && ni < N && nj < N) {
-						diff = Math.abs(map[i][j]-map[ni][nj]);
-						if(diff >= L && diff <= R) {
-							System.out.println(i+" / "+j);
-							isStop = false;
-							bfs(ni, nj, idx);
-						}
+					sum = 0;
+					count = 0;
+					solve(i,j);
+					if(count <= 1) {	//하나만 방문했을 때 맵리셋 필요 x
+						visited[i][j] = newVisited[i][j];
+					}else {
+						resetMap();
 					}
 				}
-				//System.out.println();
 			}
+			result++;
+			if(temp == change) break;
 		}
-		return isStop;
-	}
 	
-	private static void bfs(int i, int j, int idx) {
-		double people = 0;
-		int cnt = 0;
-		int diff;
-		
-		Queue<Dot> queue = new LinkedList<Dot>();
-		queue.add(new Dot(i,j));
-		visited[i][j] = idx;
-		people += map[i][j];
-		cnt++;
-		
-		//연합 생성
-		Dot cur;
-		int ni,nj;
-		int size;
-		
-		while(!queue.isEmpty()) {
-			//people = 0;
-			//cnt = 0;
-			size = queue.size();
-			while(size --> 0) {
-				cur = queue.poll();
-				//people += map[cur.i][cur.j];
-				//cnt++;
-				
-				for(int d=0; d<4; d++) {
-					ni = cur.i + dir[d][0];
-					nj = cur.j + dir[d][1];
-					
-					if(ni >= 0 && nj >= 0 && ni < N && nj < N && visited[ni][nj] == 0) {
-						diff = Math.abs(map[cur.i][cur.j]-map[ni][nj]);
-						if(diff >= L && diff <= R) {
-							people += map[ni][nj];
-							cnt++;
-							visited[ni][nj] = idx;
-							queue.add(new Dot(ni,nj));
-						}
-					}
-				}				
-			}
-			//연합 인구수 구하기
-			int result = (int)Math.floor(people/cnt);
-			
-			//인구이동
-			move(result, idx);
-			people = 0;
-			cnt = 0;
-			idx--;
-		}
-		ans++;
+		System.out.println(result-1);
+	}
+
+
+	private static void resetMap() {
+		// TODO Auto-generated method stub
 		
 	}
 
-	private static void move(int result, int idx) {
-		for(int v=0; v<N; v++) {
-			System.out.println(Arrays.toString(visited[v]));
-		}
-		
-//		for (int i = 0; i < N; i++) {
-//			for (int j = 0; j < N; j++) {
-//				System.out.print(map[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-		System.out.println();
-		//visited = true인 나라들만 바꿔주기
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(visited[i][j] == idx) {
-					map[i][j] = result;
-				}
-			}
-		}
-	}
 
+	private static void solve(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
