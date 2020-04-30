86ea6881c4404e13e153ed2594eadeab9c2490c3
이수연 <qkdqnwpwp@naver.com>
2020-05-01 01:01:44 +0900

    200430 | 백준_G5_16234_인구이동

--- Algorithm/src/com/java/algo/baekjoon/Main_B_G5_16234_인구이동.java
+++ Algorithm/src/com/java/algo/baekjoon/Main_B_G5_16234_인구이동.java
@@ -2,9 +2,6 @@ package com.java.algo.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_B_G5_16234_인구이동 {
@@ -19,75 +16,92 @@ public class Main_B_G5_16234_인구이동 {
 	private static int count;	//이동한 횟수
 	private static int sum, result, change;
 	
-	private static class Dot{
-		int i,j;
-		Dot(int i, int j){
-			this.i = i;
-			this.j = j;
-		}
-	}
 	
-	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
+
 		map = new int[N][N];
-		visited = new boolean[N][N];
-		newVisited = new boolean[N][N];
 		count = 0;
 		sum = 0;
 		result = 0;
 		change = 0;
-	
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0; j<N; j++) {
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		//dfs
-		while(true) {
+
+		while (true) {
 			int temp = change;
 			visited = new boolean[N][N];
 			newVisited = new boolean[N][N];
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(newVisited[i][j] == true) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (newVisited[i][j] == true) {
 						continue;
 					}
-					
 					sum = 0;
 					count = 0;
-					solve(i,j);
-					if(count <= 1) {	//하나만 방문했을 때 맵리셋 필요 x
+					solve(i, j);
+					if (count <= 1) { // 하나만 방문했을때 맵리셋 필요 x
 						visited[i][j] = newVisited[i][j];
-					}else {
+					} else {
 						resetMap();
 					}
 				}
 			}
+			
 			result++;
+//			for (int i = 0; i < N; i++) {
+//				for (int j = 0; j < N; j++) {
+//					System.out.print(map[i][j]+" ");
+//				}System.out.println();
+//			}
 			if(temp == change) break;
 		}
-	
-		System.out.println(result-1);
+
+		System.out.println(result - 1);
 	}
 
+	// dfs해서 true로 만듬 - true로 바뀌는 경우가 없을때 끝!
+	static void solve(int x, int y) {
+		newVisited[x][y] = true;
+		sum += map[x][y];
+		count++;
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dir[d][0];
+			int ny = y + dir[d][1];
+			if (nx < 0 || nx > N - 1 || ny < 0 || ny > N - 1)
+				continue;
+			if (newVisited[nx][ny] == true)
+				continue;
+			int temp = Math.abs(map[x][y] - map[nx][ny]);
+			if (temp >= L && temp <= R) {
+				solve(nx, ny);
+			}
 
-	private static void resetMap() {
-		// TODO Auto-generated method stub
-		
+		}
+
 	}
 
-
-	private static void solve(int i, int j) {
-		// TODO Auto-generated method stub
-		
+	// true인걸 골라서 sum 구하고 다시 넣어주기
+	static void resetMap() {
+		int avg = sum / count;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (visited[i][j] != newVisited[i][j]) {
+					map[i][j] = avg;
+					change++;
+				}
+				visited[i][j] = newVisited[i][j];
+			}
+		}
 	}
 }
 
